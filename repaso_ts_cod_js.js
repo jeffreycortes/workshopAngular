211455271d354a3fc924d1380f31fd478ba5d0a2
Jeffrey Alberto Cortes Cuesta <ext_jefcortes@Bancofalabella.com.co>
2020-01-31 14:20:34 -0500

    Se adicionan los archivos javascript y typescript para el comparativo y repaso de TS.

---
+++ repaso_ts_cod_js.js
@@ -0,0 +1,28 @@
+// Javascript
+// Declaración de variables
+var x = 0;
+let y = "dos";
+const z = [];
+// cuando no hay cambio de asignación del objeto
+const obj = {id: 1, value: "Hola Mundo!"};
+obj.id = 2;
+
+//funciones
+function inicializar() {
+  // lógica
+}
+
+function saludo() {
+  return "Hola Mundo";
+}
+
+function operacion(x , y) {
+  return x + y;
+}
+
+// Array desestructuración
+const arr:Array<any> = [false, 1, "2", 3];
+let [first, second] = arr;
+let [,,...dc] = arr;
+console.log(first, second);
+console.log(dc);
