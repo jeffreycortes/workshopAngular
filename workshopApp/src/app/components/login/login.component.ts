e02dc3e7904b94b89342cbacfdcb000b1e668814
Jeffrey Alberto Cortes Cuesta <ext_jefcortes@Bancofalabella.com.co>
2020-02-18 17:28:15 -0500

    Se agregan métodos y propiedades al componente "login" para implementar data binding

--- workshopApp/src/app/components/login/login.component.ts
+++ workshopApp/src/app/components/login/login.component.ts
@@ -5,10 +5,51 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  // Propiedades
+  public login: string;
+  public password: string;
+  public readonly TOKEN_AUTENTICACION = 'token_on';
+  public listaEventosSignIn: IEventoSignIn[];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.login = '';
+    this.listaEventosSignIn = [];
   }
+
+  signIn(): void {
+    const mensaje =
+      this.autenticarUsuario(this.login, this.password) ?
+      'Usuario autenticado!!' :
+      'Error: !Credenciales incorrectas!';
+
+    alert(mensaje);
+  }
+
+  autenticarUsuario(login: string, password: string): boolean {
+    this.registrarEventoDeSignIn(login);
+    return (password === this.TOKEN_AUTENTICACION);
+  }
+
+  registrarEventoDeSignIn(login: string): void {
+    this.listaEventosSignIn.push({fechaSignIn: new Date(), login});
+  }
+
+  keypress(evt: any): void {
+    const charCode = evt.charCode || evt.keyCode;
+
+    if (charCode === 13) {
+      this.signIn();
+    }
+  }
+
+}
+
+interface IEventoSignIn {
+  fechaSignIn: Date;
+  login: string;
 }
